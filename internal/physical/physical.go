6b0dfe50fab80b65a878639574d56979ed126c32
Mayvid0 <divbillowria@gmail.com>
2024-03-23 19:08:14 +0530

    created basic device struct

--- internal/physical/physical.go
+++ internal/physical/physical.go
@@ -1,1 +1,21 @@
 package physical
+
+// there should be a struct for device (doesnt matter what layer... a basic struct
+// all other layer devices can inherit from it)
+
+type Device struct {
+	Name       string
+	MACAddress string
+	LinkStatus bool
+}
+
+type Hub struct {
+	Device
+	NumberPorts int
+	EndDevices  []*Device
+}
+
+type Connection struct {
+	Device1 *Device
+	Device2 *Device
+}
